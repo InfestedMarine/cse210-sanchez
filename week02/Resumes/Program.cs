267ba090dcae1af22ce119415aed2e41a2bb3a4a
InfestedMarine <cyandsanchez@gmail.com>
2025-01-17 13:33:40 +0800

    Did my Resume

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        job job1 = new job();
+        job1._jobTitle = "Level Designer";
+        job1._company = "Valve.Corp";
+        job1._startYear = 2005;
+        job1._endYear = 2015;
+
+        Resume myResume = new Resume();
+        myResume._name = "Cyan D. Sanchez";
+
+        job job2 = new job();
+        job2._jobTitle = "Software Engineer";
+        job2._company = "Microsoft";
+        job2._startYear = 2019;
+        job2._endYear = 2022;
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+        myResume.Display();
     }
 }
